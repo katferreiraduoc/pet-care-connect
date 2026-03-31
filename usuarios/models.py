e89a15de6e186a10a8dcfa206934ebdeee13e1f3
Kat Ferreira <kat.ferreira@duocuc.cl>
2026-03-31 10:17:55 -0300

    fix imports models

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Rol(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
@@ -6,8 +7,6 @@ class Rol(models.Model):
 
     def __str__(self):
         return self.nombre
-
-from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
     rol = models.ForeignKey(Rol, on_delete=models.PROTECT, null=True, blank=True)
