60e22d4c4c8f6912da7e90f40f599d3d24f3de32
Kat Ferreira <kat.ferreira@duocuc.cl>
2026-03-31 10:10:11 -0300

    add model mascota

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Rol, Usuario
+from .models import Rol, Usuario, Mascota
 
 admin.site.register(Rol)
 admin.site.register(Usuario)
+admin.site.register(Mascota)
--- usuarios/models.py
+++ usuarios/models.py
@@ -12,3 +12,20 @@ from django.contrib.auth.models import AbstractUser
 class Usuario(AbstractUser):
     rol = models.ForeignKey(Rol, on_delete=models.PROTECT, null=True, blank=True)
     telefono = models.CharField(max_length=20, blank=True, null=True)
+
+class Mascota(models.Model):
+    SEXO_CHOICES = [
+        ('macho', 'Macho'),
+        ('hembra', 'Hembra'),
+    ]
+
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    especie = models.CharField(max_length=50)
+    raza = models.CharField(max_length=80, blank=True, null=True)
+    sexo = models.CharField(max_length=10, choices=SEXO_CHOICES)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    color = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
