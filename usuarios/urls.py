29e05c7a67b2d453fb19daf9dd7bde25eb7d7956
Kat Ferreira <kat.ferreira@duocuc.cl>
2026-04-16 15:43:20 -0400

    fix urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import LogoutView
 from .views import CustomLoginView, home
 
 urlpatterns = [
-    path('', home, name='home'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('', home, name='home'),
 ]
