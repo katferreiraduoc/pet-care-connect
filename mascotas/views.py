16023a0d82298a0d8cf5d5397f00b39be8eba0cd
gberriosgarcia <gerk.berrios@duocuc.cl>
2026-04-15 20:05:09 -0400

    se añadieron paginas en blanco del menu

--- mascotas/views.py
+++ mascotas/views.py
@@ -5,3 +5,18 @@ def home(request):
 
 def agregar_mascota(request):
     return render(request, 'mascota_add.html')
+
+def mis_mascotas(request):
+    return render(request, 'mis_mascotas.html')
+
+def citas(request):
+    return render(request, 'citas.html')
+
+def registros_medicos(request):
+    return render(request, 'registros_medicos.html')
+
+def dieta(request):
+    return render(request, 'dieta.html')
+
+def panel_control(request):
+    return render(request, 'panel_control.html')
--- petcare/urls.py
+++ petcare/urls.py
@@ -2,11 +2,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from mascotas.views import home, agregar_mascota
+from mascotas.views import dieta, home, agregar_mascota, mis_mascotas, citas, panel_control, registros_medicos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('agregar-mascota/', agregar_mascota, name='agregar_mascota')
-    
+    path('agregar-mascota/', agregar_mascota, name='agregar_mascota'),
+    path('mis_mascotas/', mis_mascotas, name='mis_mascotas'),
+    path('citas/', citas, name='citas'),
+    path('registros-medicos/', registros_medicos, name='registros_medicos'),
+    path('dieta/', dieta, name='dieta'),
+    path('panel-control/', panel_control, name='panel_control'),        
 ]
+
+
